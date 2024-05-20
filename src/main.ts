f33c74353b5235aad5c6a123fa3b00a91ac0ca74
DevAlberto <albertoulin619@gmail.com>
2024-05-19 18:51:57 -0600

    nueva ruta

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     origin: [
       'http://panaderia-dev.s3-website.us-east-2.amazonaws.com', // Frontend en S3
       'http://localhost:3000', // Localhost para pruebas locales
+      'http://localhost:4200', // Localhost para pruebas locales
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
